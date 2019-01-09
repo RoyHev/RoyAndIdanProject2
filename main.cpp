225c4c2e137382a357083c38fa2b2b4d571ad321
idantp <idantp@gmail.com>
2019-01-09 03:09:57 +0200

    update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 #include "Point.h"
-#include "Matrix.h"
-
+//#include "Matrix.h"
+#include "Searcher.h"
+#include "CacheManager.h"
+#include "ClientHandler.h"
+#include "DepthFirstSearch.h"
+#include "Server.h"
+#include "Solver.h"
 int main() {
+//    Searcher<Point> *searcher = new Searcher()
 
 }
