e259cf3e6703a8200f706287058f1b808b180482
idantp <idantp@gmail.com>
2019-01-13 14:55:20 +0200

    SeverCommunication

--- ServerCommunication.cpp
+++ ServerCommunication.cpp
@@ -2,4 +2,49 @@
 // Created by roy on 13/01/19.
 //
 
+#include <strings.h>
+#include <cstring>
+#include <sys/socket.h>
 #include "ServerCommunication.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void ServerCommunication::writeToServer(int socketNum, string info) {
+    //TODO - delete if not needed (works properly).
+//    char buffer[SIZE];
+//    bzero(buffer, SIZE);
+//    strcpy(buffer, info.c_str());
+    /* Send message to the server */
+//    ssize_t n = write(socketNum, buffer, strlen(buffer));
+    ssize_t n = write(socketNum, info.c_str(), (info.size()));
+    if (n < 0) {
+        perror("Could not write to Server.");
+        exit(1);
+    }
+}
+
+
+vector<string> ServerCommunication::readFromServer(int socketNum) {
+    vector<string> problemInfo;
+    char c;
+    string temp;
+
+    while (temp != END_PROBLEM)
+    {
+        ssize_t n = read(socketNum, &c, READ_CHAR);
+        if (n < 0) {
+            perror("Could not read from Server.");
+        }
+        while(c != ENTER){
+            temp += c;
+            read(socketNum, &c, READ_CHAR);
+        }
+        if (temp == END_PROBLEM){
+            break;
+        }
+        problemInfo.push_back(temp);
+        temp.erase();
+    }
+    return problemInfo;
+}
--- ServerCommunication.h
+++ ServerCommunication.h
@@ -5,9 +5,22 @@
 #ifndef ROYANDIDANPROJECT2_SERVERCOMMUNICATION_H
 #define ROYANDIDANPROJECT2_SERVERCOMMUNICATION_H
 
+#include <string>
+#include <vector>
+
+#define SIZE 1024
+#define READ_CHAR 1
+#define END_PROBLEM "end"
+#define ENTER '\n'
+using namespace std;
 
 class ServerCommunication {
+public:
+    ServerCommunication() = default;
 
+    vector<string> readFromServer(int socketNum);
+
+    void writeToServer(int socketNum, string info);
 };
 
 
