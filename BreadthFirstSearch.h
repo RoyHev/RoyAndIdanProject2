997c67e85a4bb921cb08baf321be6c3c20563b34
Roy Hevrony <Roy.hevrony@gmail.com>
2019-01-14 20:02:45 +0200

    Adding Comments to the Code.

--- BreadthFirstSearch.h
+++ BreadthFirstSearch.h
@@ -9,10 +9,17 @@
 #include <queue>
 #include <iostream>
 
+using namespace std;
+
 template<class T>
 class BreadthFirstSearch : public Searcher<T> {
 public:
-
+    /**
+     * Checks if the node has already been visited during the BFS scan.
+     * @param nodesVec - vector of nodes that have been visited.
+     * @param node - node to check.
+     * @return true / false .
+     */
     bool hasNodeBeenVisited(vector<State<T> *> nodesVec, State<T> *node) {
         for (State<T> *state : nodesVec) {
             if (state->equalsTo(node)) {
@@ -21,41 +28,49 @@ public:
         }
         return false;
     }
-
+    //BFS algorithm.
     vector<State<T> *> search(Searchable<T> *searchable) override {
         vector<State<T> *> nodesVisited;
-        vector<State<T> *> tempVec;
         vector<State<T> *> path;
+        //set the current state to the initial we received from the problem.
         State<T> *currentState = searchable->getInitialState();
         currentState->setCameFrom(currentState);
+        //put it into the nodes that have been visited.
         nodesVisited.emplace_back(currentState);
         State<T> *goalState = searchable->getGoalState();
         queue<State<T> *> stateQ;
         stateQ.push(currentState);
+        //go over the state queue until it is empty.
         while (!stateQ.empty()) {
 
             currentState = stateQ.front();
+            //the node we are currently on is the goal node.
             if (currentState->equalsTo(goalState)) {
                 path.insert(path.begin(), currentState);
+                //restores the path from the initial to the goal.
                 while (!(currentState->equalsTo(
                         searchable->getInitialState()))) {
                     currentState = currentState->getCameFrom();
                     path.insert(path.begin(), currentState);
                 }
+                //returns the path.
                 return path;
             }
             stateQ.pop();
+            //goes over all sons of the current state and expands them.
             for (State<T> *adj : searchable->getPossibleStates(currentState)) {
+                //already visited, continue.
                 if (hasNodeBeenVisited(nodesVisited, adj)) {
                     continue;
                 }
+                //otherwise, insert to the nodes visited , set it's cameFrom and insert into state queue.
                 nodesVisited.emplace_back(adj);
                 adj->setCameFrom(currentState);
                 stateQ.push(adj);
             }
         }
         //could not find path from requested initial to goal.
-        return tempVec;
+        return path;
 
     }
 
